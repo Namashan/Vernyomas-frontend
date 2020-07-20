98edb421de8e78d56a07a3911c5d86462a530322
Teke Máté <mate.teke@gmail.com>
2020-07-20 13:35:02 +0200

    naplofeligkesz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppRoutingModule } from './app.routing';
@@ -16,6 +20,7 @@ import {
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ForumComponent } from './Forum/forum/forum.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   imports: [
@@ -26,6 +31,10 @@ import { ForumComponent } from './Forum/forum/forum.component';
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,}),
     AgmCoreModule.forRoot({
       apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
     }),
@@ -35,6 +44,7 @@ import { ForumComponent } from './Forum/forum/forum.component';
     AppComponent,
     AdminLayoutComponent,
     ForumComponent,
+    CalendarComponent,
 
   ],
   providers: [],
